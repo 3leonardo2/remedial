8b7af9015af7067e4961b1f6e0edc958d2aac5e3
Leonardo <leonardo.alatorre.23s@utzmg.edu.mx>
2025-03-18 08:37:25 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
-import React, {  useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Formulario from './components/Formulario';
-import IntentoSesion from './components/IntentoSesion';
-
-const Stack = createStackNavigator();
-
-const App = () => {
-
-    useEffect(() => { // Verificar si hay una sesión activa
-        const checkSession = async () => { // Función para verificar la sesión
-            const token = await AsyncStorage.getItem('userToken'); // Obtener el token de usuario
-            setIsLoggedIn(!!token); // Actualizar el estado de la sesión
-        };
-
-        checkSession(); // Verificar si hay una sesión activa
-    }, []);
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                    <Stack.Screen name="Formulario" component={Formulario} />
-                    <Stack.Screen name="IntentoSesion" component={IntentoSesion} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
-
-export default App;
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import axios from "axios";
+export default function App() {
+  const handleLEDOn = async () => {
+    try {
+      await axios.get("http://192.168.1.123/led/on"); // Reemplaza con tu IP real
+      Alert.alert("LED Encendido");
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error encendiendo el LED");
+    }
+  };
+  const handleLEDOff = async () => {
+    try {
+      await axios.get("http://192.168.1.123/led/off"); // Reemplaza con tu IP real
+      Alert.alert("LED Apagado");
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error apagando el LED");
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Control de LED con ESP32</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Encender LED" onPress={handleLEDOn} />
+      </View>
+      <View style={styles.buttonContainer}></View>
+      <Button title="Apagar LED" onPress={handleLEDOff} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  buttonContainer: {
+    marginVertical: 10,
+  },
+});
